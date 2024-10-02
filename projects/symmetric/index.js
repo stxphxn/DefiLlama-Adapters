8baec3f59112617d7f52e905c806c268d4f9dfa8
stephen <991266+stxphxn@users.noreply.github.com>
2024-10-02 18:30:52 +0100

    fix graphEndpoints

--- projects/symmetric/index.js
+++ projects/symmetric/index.js
@@ -14,7 +14,7 @@ async function tvlFromGraph(network) {
     }
   }
   `
-  const res = await request(graphEndpoint[network], graphQuery);
+  const res = await request(graphEndpoints[network], graphQuery);
   return res.balancers[0].totalLiqudity
 }
 
